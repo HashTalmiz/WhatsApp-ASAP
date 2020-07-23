ecc158540813b3327fbb0a33dbaec34632633457
HashTalmiz <talmiz1234@gmail.com>
2020-07-23 23:59:34 +0530

    Completed submit handling with success and error message

--- src/components/MainBody.vue
+++ src/components/MainBody.vue
@@ -6,11 +6,14 @@
       <h1><i class="fa fa-whatsapp" style="color:green" aria-hidden="true"></i>   WhatsApp ASAP</h1><!-- /ASAP/ No Save-->
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure tenetur, corporis nostrum maxime inventore illum eligendi nobis mollitia id ex?</p>
       <div>
-        +<input type="number" id="cCode" v-model="user.countryCode" maxlength="2" class="form-control" />
-        <input type="number" id="phoneNumber" v-model="user.phoneNumber" maxlength="10" class="form-control"/>
+        +<input type="number" id="cCode" v-model="user.countryCode" @focus="clearStatus" maxlength="2" class="form-control" />
+        <input type="number" id="phoneNumber" v-model="user.phoneNumber" @focus="clearStatus" maxlength="10" class="form-control"/>
       </div>
-      <p v-if="error && submitting" class="error-message">
+      <p v-if="error" class="error-message">
           ❗Please enter a 10 digit number with a 2 digit country code
+      </p>
+      <p v-if="success" class="success-message">
+          ✅ Success! Make sure you don't have pop ups blocked!
       </p>
       <button type="button" class="accent-button" @click="handleSubmit" >Start Chat</button>
     </div>
@@ -38,8 +41,8 @@ export default {
   name: 'MainBody',
   data() {
     return {
-      error: true,
-      submitting: true,
+      error: false,
+      success: false,
       user : {
         phoneNumber: '',
         countryCode: '91'
@@ -47,6 +50,12 @@ export default {
     }
   },
   computed: {
+    validCountryCode() {
+      return this.user.countryCode.length === 2;
+    },
+    validPhoneNumber() {
+      return this.user.phoneNumber.length === 10;
+    },
     iPhoneOutput() {
       if(this.user.countryCode<=2 && this.user.phoneNumber<=10)
         return this.user.countryCode+' '+this.user.phoneNumber;
@@ -58,10 +67,21 @@ export default {
     }
   },
   methods: {
-      handleSubmit(){
-      },
-      openWhatsapp: function(){
-       window.open(`${this.redirectLink}`);
+    clearStatus() {
+      this.error=false;
+      this.success=false;
+    },
+    handleSubmit(){
+      this.clearStatus();
+      if(this.validCountryCode && this.validPhoneNumber)
+      {
+        this.success = true;
+        this.openWhatsapp();
+      }else
+        this.error=true;
+    },
+    openWhatsapp: function(){
+      window.open(`${this.redirectLink}`);
     }
   }
  
